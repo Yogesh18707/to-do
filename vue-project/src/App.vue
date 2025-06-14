b6bdfb0504f8ae56a68f684f379f693aba0eecda
Yogesh <yogesh628882@gmail.com>
2025-06-14 10:55:13 +0530

    i

---
+++ vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="main">
+    <div class="container">
+      <div class="heading">
+        <div>To-Do List</div>
+      </div>
+      <div class="todo"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div class="hello">
+
+ </div>
+</template>
+
+<style scoped>
+
+</style>
